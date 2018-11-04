051527c265c0760fa4807fa7d4493ee791e17109
Breno Gregório Angelotti <breno@forlogic.net>
2018-11-04 21:32:51 -0200

    Updates

---
+++ precache-manifest.c326c12d8195c200df139d5cce03f544.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "ee2a100325fd284f0097",
+    "url": "/portfolio/static/js/runtime~main.ee2a1003.js"
+  },
+  {
+    "revision": "648799f712bdaa0ed454",
+    "url": "/portfolio/static/js/main.648799f7.chunk.js"
+  },
+  {
+    "revision": "3a58f11f13f5574bd1fa",
+    "url": "/portfolio/static/js/1.3a58f11f.chunk.js"
+  },
+  {
+    "revision": "648799f712bdaa0ed454",
+    "url": "/portfolio/static/css/main.3aebfbd1.chunk.css"
+  },
+  {
+    "revision": "9e27ffffb6fb3381326082073c839592",
+    "url": "/portfolio/index.html"
+  }
+];
